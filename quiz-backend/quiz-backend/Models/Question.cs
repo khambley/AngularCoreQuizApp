7782f97faac226777388eb3d7b2bb898035d9096
Katherine Hambley <superdesigngirl@mac.com>
2020-05-12 21:19:44 -0500

    added answers input to question in Angular

--- quiz-backend/quiz-backend/Models/Question.cs
+++ quiz-backend/quiz-backend/Models/Question.cs
@@ -9,6 +9,12 @@ namespace quiz_backend.Models
 	{
 		public int Id { get; set; }
 		public string Text { get; set; }
+		public string CorrectAnswer { get; set; }
+		public string Answer1 { get; set; }
+		public string Answer2 { get; set; }
+		public string Answer3 { get; set; }
+
+
 	}
 
 }
